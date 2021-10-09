dbddb8b29b73025a2a6ed9251e1648e8e32ecda3
Carl Santiago <4518gg@gmail.com>
2021-10-10 09:11:40 +1030

    Added CSS and working clock

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var button = $('.saveBtn')
+
+// current time and date
+var datetime = null,
+        date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#currentDay')
+    update();
+    setInterval(update, 1000);
+});
+
