0b25b6be49acc8d6586e4e4154201906e6607760
Carl Santiago <4518gg@gmail.com>
2021-10-10 18:24:46 +1030

    Added past present future js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 var button = $('.saveBtn')
 
-// current time and date
+// current time and
 var datetime = null,
         date = null;
 
@@ -15,3 +15,27 @@ $(document).ready(function(){
     setInterval(update, 1000);
 });
 
+// check if current time is past, present or future
+
+  function currentTime() {
+    var current = moment().hours()
+    var time = $('.time');
+
+    time.each(function () {
+      var hour = parseInt($(this).attr('id'))
+
+      if (hour === current) {
+        $(this).children('.col-sm-10').attr('class', 'present col-sm-10 description')
+
+      } else if (current > hour) {
+        $(this).children('.col-sm-10').attr('class', 'past col-sm-10 description')
+
+      } else {
+        $(this).children('.col-sm-10').attr('class', 'future col-sm-10 description')
+
+      }
+    })
+  }
+
+  
+  currentTime()
